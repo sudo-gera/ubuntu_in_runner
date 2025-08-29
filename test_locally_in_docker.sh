ceb4affcf47b673e0b30a6bc37afe7c0b267cfe5
sudo-gera <gera.tatarinov.999@gmail.com>
2025-08-30 02:19:57 +0300

    *interesting commit message*

--- test_locally_in_docker.sh
+++ test_locally_in_docker.sh
@@ -8,18 +8,18 @@ cd "$(
     )"
 )"
 
-(
-    printf '%s\n' 'FROM ubuntu:latest'
+# (
+#     printf '%s\n' 'FROM ubuntu:latest'
 
-    jq -c '.[]' <<< "$(cat .github/workflows/.gitlab-ci.yml | jq '.jobs.build.steps[].run | select(.)' | jq -s)" | while IFS= read -r item; do
-        if [ "$item" = '":"' ]
-        then
-            printf '%s\n' 'COPY . .'
-        else
-            printf 'RUN '
-            jq -c '["sh", "-c", .]' <<< "$item"
-        fi
-    done
-) > Dockerfile
+#     jq -c '.[]' <<< "$(cat .github/workflows/.gitlab-ci.yml | jq '.jobs.build.steps[].run | select(.)' | jq -s)" | while IFS= read -r item; do
+#         if [ "$item" = '":"' ]
+#         then
+#             printf '%s\n' 'COPY . .'
+#         else
+#             printf 'RUN '
+#             jq -c '["sh", "-c", .]' <<< "$item"
+#         fi
+#     done
+# ) > Dockerfile
 
 docker build --progress=plain -t ubuntu-in-runner .
